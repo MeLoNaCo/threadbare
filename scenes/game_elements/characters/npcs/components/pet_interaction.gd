549de527c49a23d89a9f8fa0c0deb515c65e4930
GarayLlajaLeonardo <bruno951357@gmail.com>
2026-09-22 18:18:22 -0500

    Sheep annoy animation

--- scenes/game_elements/characters/npcs/components/pet_interaction.gd
+++ scenes/game_elements/characters/npcs/components/pet_interaction.gd
@@ -14,12 +14,17 @@ extends Node
 ## If set, it will increment when interacted.
 @export var fact_counter: FactCounter
 
+## If set, it will play the sheep animation when interacted.
+@export var pet_animation: AnimatedSprite2D
+
+@export var pet_sprite_behavior: CharacterSpriteBehavior
+
 
 func _ready() -> void:
 	interact_area.interaction_started.connect(_on_interact_area_interaction_started)
 
 
-func _on_interact_area_interaction_started(_player: Player, _from_right: bool) -> void:
+func _on_interact_area_interaction_started(player: Player, _from_right: bool) -> void:
 	interact_area.end_interaction()
 	interact_area.disabled = true
 	sound_effect_player.play()
@@ -27,5 +32,12 @@ func _on_interact_area_interaction_started(_player: Player, _from_right: bool) -
 		shaker.shake()
 	if fact_counter:
 		fact_counter.increment()
-	await sound_effect_player.finished
+	var player_is_on_right := player.global_position.x > pet_animation.global_position.x
+	var base_sprite_looks_left := pet_animation.global_transform.x.x < 0.0
+	pet_animation.flip_h = player_is_on_right != base_sprite_looks_left
+	pet_animation.play(&"annoy")
+	await pet_animation.animation_finished
+	pet_animation.play(&"idle")
+	if sound_effect_player.playing:
+		await sound_effect_player.finished
 	interact_area.disabled = false
